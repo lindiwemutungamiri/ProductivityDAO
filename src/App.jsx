606bbf276038208016728dd9b0376530bb436f64
Lindiwe Mutungamiri <lindiwemutungamiri23@gmail.com>
2022-04-18 16:57:14 +0000

    changed the dashboard name

--- src/App.jsx
+++ src/App.jsx
@@ -183,7 +183,7 @@ const App = () => {
   if (!address) {
     return (
       <div className="landing">
-        <h1>Welcome to NarutoDAO</h1>
+        <h1>Welcome to the ProductivityDAO</h1>
         <button onClick={connectWithMetamask} className="btn-hero">
           Connect your wallet
         </button>
